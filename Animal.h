b232c8955c60fbb8d928de504f25c2549ed3671a
MMahmud24 <mahinmahmud2406@gmail.com>
2025-08-26 18:26:22 -0400

    separating zoo.cpp file

---
+++ Animal.h
@@ -0,0 +1,18 @@
+#include <string>
+
+class Animal{
+    public:
+        Animal(std::string speciesName, unsigned int discoveryYear){
+            species = speciesName;
+            year_discovered = discoveryYear;
+        }
+
+        Animal();
+
+        void display();
+
+    private:
+        std::string species = "";
+        unsigned int year_discovered = 0;
+
+}
